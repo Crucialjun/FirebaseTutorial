976547c9a1c007df042fc0f938e9bae3d23f4125
Crucialjun <nich.otieno@gmail.com>
2018-11-27 23:01:10 +0300

    Read Data from Database

--- app/src/main/java/com/example/crucialjun/firebasetutorial/MainActivity.java
+++ app/src/main/java/com/example/crucialjun/firebasetutorial/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.crucialjun.firebasetutorial;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText childValueEditText;
     Button addButton;
     Button removeButton;
+    private TextView childValueTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         final DatabaseReference mRef = firebaseDatabase.getReference("crucial");
 
+        childValueTextView = findViewById(R.id.childValueTextView);
         childValueEditText = findViewById(R.id.childValueEditText);
         addButton = findViewById(R.id.addButton);
         removeButton = findViewById(R.id.removeButton);
@@ -39,6 +46,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mRef.removeValue();
+            }
+        });
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String childVaue = String.valueOf(dataSnapshot.getValue());
+                childValueTextView.setText(childVaue);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
